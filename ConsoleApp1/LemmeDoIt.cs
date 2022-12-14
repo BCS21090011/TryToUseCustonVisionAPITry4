f414d2e975d0c4e1f076a5519e3fce2c707abff6
BCS21090011 <bcs21090011@student.bcs.edu.my>
2022-12-14 09:20:36 +0800

    Created.

---
+++ ConsoleApp1/LemmeDoIt.cs
@@ -0,0 +1,115 @@
+﻿namespace LemmeDoIt
+{
+
+    using System.Drawing;
+    using System.IO;
+    using System.Xml.Linq;
+    using System;
+    using Newtonsoft.Json;
+    using System.Net.Http.Headers;
+    using ConsoleApp1.Models;
+    using System.Drawing;
+
+    public class LemmeProcess
+    {
+
+        string predictionKey = "35532dc500874d7a86cf0e18b789bc5a";
+        string predictionUrl = "https://yysprediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/f79b57d5-32a9-4566-949d-b144204c7ae0/detect/iterations/Iteration4/image";
+
+        public async void GimmeImg(string imgFileName, float passProbability, string saveFileName = "")
+        {
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Prediction-Key", predictionKey);
+            var file = File.OpenRead(imgFileName);
+            byte[] imgBytes = FileReader.ReadFully(file);
+            var content = new ByteArrayContent(imgBytes);
+            Bitmap oriImg = (Bitmap)Image.FromFile(imgFileName);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+            try
+            {
+
+                var res = await client.PostAsync(predictionUrl, content);
+                var str = await res.Content.ReadAsStringAsync();
+                var myPredictionModel = JsonConvert.DeserializeObject<MyPredictionModel>(str);
+                var predictions = myPredictionModel.predictions;
+
+                int index = 0;
+                if (predictions != null)
+                {
+                    foreach (Prediction prediction in predictions)
+                    {
+                        if (prediction.probability >= passProbability)
+                        {
+                            if (prediction.tagName == "people")
+                            {
+
+                                //string cleanFileName = Path.GetFileNameWithoutExtension(imgFileName);
+                                string outputFileName = "../../../Outputs/" + saveFileName + "Output" + index + ".jpg";
+
+                                Console.WriteLine($"File: {saveFileName}");
+                                Console.WriteLine($"Prediction number {index}:");
+                                Console.WriteLine($"{prediction.tagName}:{prediction.probability}");
+                                Console.WriteLine("Bounding box:");
+                                Console.WriteLine($"Left: {prediction.boundingBox.left}\tTop: {prediction.boundingBox.top}");
+                                Console.WriteLine($"Width: {prediction.boundingBox.width}\tHeight: {prediction.boundingBox.height}");
+
+                                Rectangle rect = GetRect(oriImg, prediction.boundingBox);
+                                Bitmap peopleImg = CutImg(oriImg, rect);
+                                peopleImg.Save(outputFileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                                index++;
+
+                            }
+                        }
+                    }
+
+                }
+
+            }
+            catch (Exception e)
+            {
+
+            }
+
+        }
+
+        Rectangle GetRect(Bitmap img, BoundingBox bdBox)
+        {
+            int calcedLeft = Convert.ToInt32(img.Width * bdBox.left);
+            int calcedTop = Convert.ToInt32(img.Height * bdBox.top);
+            int calcedWidth = Convert.ToInt32(img.Width * bdBox.width);
+            int calcedHeight = Convert.ToInt32(img.Height * bdBox.height);
+
+            Rectangle ret = new Rectangle(calcedLeft, calcedTop, calcedWidth, calcedHeight);
+
+            return ret;
+        }
+
+        Bitmap CutImg(Bitmap img, Rectangle ruler)
+        {
+            Bitmap cuttedImg = img.Clone(ruler, img.PixelFormat);
+
+            return cuttedImg;
+        }
+
+        public class FileReader
+        {
+            public static byte[] ReadFully(Stream input)
+            {
+                byte[] buffer = new byte[16 * 1024];
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    int read;
+                    while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        ms.Write(buffer, 0, read);
+                    }
+                    return ms.ToArray();
+                }
+            }
+        }
+
+    }
+
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,57 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Drawing;
-using System.IO;
-using System.Xml.Linq;
-using System;
-using Newtonsoft.Json;
-using System.Net.Http.Headers;
-using ConsoleApp1.Models;
-
-var predictionKey = "";
-var predictionUrl = "";
-HttpClient client = new HttpClient();
-client.DefaultRequestHeaders.Add("Prediction-Key", predictionKey);
-var file = File.OpenRead("img.jpg");
-byte[] imgBytes = FileReader.ReadFully(file);
-using (var content = new ByteArrayContent(imgBytes))
+﻿namespace Program
 {
-    // request API 
-    content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
-    try
-    {
-        var res = await client.PostAsync(predictionUrl, content);
-        var str = await res.Content.ReadAsStringAsync();
-        var myPredictionModel = JsonConvert.DeserializeObject<MyPredictionModel>(str);
-        var predictions = myPredictionModel.predictions;
-        foreach (var prediction in predictions)
-        {
-            if (prediction.probability > 0.7)
-            {
-                //100px by 100px
-                //prediction.boundingBox 0.75
-            }
-        }
-    }
-    catch (Exception e)
-    {
-       
-    }
-    
-}
 
-public class FileReader
-{
-    public static byte[] ReadFully(Stream input)
+    using LemmeDoIt;
+
+    public class MainProgram
     {
-        byte[] buffer = new byte[16 * 1024];
-        using (MemoryStream ms = new MemoryStream())
+
+        public static void Main(string[] args)
         {
-            int read;
-            while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+
+            float probToPass = 0.9f;
+            string inputFolder = "../../../Inputs";
+            string[] files = Directory.GetFiles(inputFolder, "*.jpg");
+
+            LemmeProcess pred = new LemmeProcess();
+
+            int index = 0;
+            foreach (string file in files)
             {
-                ms.Write(buffer, 0, read);
+                pred.GimmeImg(imgFileName: file, passProbability: probToPass, saveFileName: Convert.ToString(index));
+                index++;
             }
-            return ms.ToArray();
+
+            Console.WriteLine("Press [Enter] to exit:");
+            Console.ReadLine();
+
         }
+
     }
+
 }
